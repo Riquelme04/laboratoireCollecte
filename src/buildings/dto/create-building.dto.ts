6c32f8125712edffc1eb48229f690004bb0a1c1c
Jonathan <6176721@collegemv.qc.ca>
2026-09-10 17:14:42 -0400

    mise a jour dto create dans building et rooms

--- src/buildings/dto/create-building.dto.ts
+++ src/buildings/dto/create-building.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateBuildingDto {
+    @ApiProperty({ 
+      description: 'Nom du bâtiment',
+      example: 'Bâtiment A'})
     name: string;
     address: string;
     yearBuilt: number;
--- src/rooms/dto/create-room.dto.ts
+++ src/rooms/dto/create-room.dto.ts
@@ -2,6 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 
 
 export class CreateRoomDto {
+  @ApiProperty({ 
+    description: 'Code du local',
+    example: 'A101'})
   code!: string;        
   buildingId!: string;  
   floor!: number;       
